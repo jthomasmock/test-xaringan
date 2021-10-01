089be023d3bb4af465a0232859f7dcfdb3936a4e
Thomas Mock <j.thomasmock@gmail.com>
2021-10-01 18:51:07 -0500

    add slides and example code

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# test-xaringan
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of test-xaringan is to test `xaringan` presentation to RStudio Connect.
+
---
+++ deploy-code.R
@@ -0,0 +1,24 @@
+library(rsconnect)
+
+# option 1, send rendered doc alone
+deployDoc("test-xaringan.html",
+          appName = "themed-xaringan",
+          appTitle = "A cool themed slide deck",
+          server = "colorado.rstudio.com")
+
+# option 2, send rendered doc alone, replace existing file
+deployDoc("test-xaringan.html",
+          server = "colorado.rstudio.com",
+          appId = "4b9ec25-9264-4ade-ba39-639bcd1dcc50")
+
+# option 3, send source file and specify files
+# with rmarkdown::find_external_resources
+# note new url, since I want this to include source version as
+# opposed to the original which was only the output HTML/css
+
+rsconnect::deployDoc("test-xaringan-sourced.Rmd",
+          server = "colorado.rstudio.com",
+          appName = "themed-xaringan-sourced",
+          appTitle = "A cool themed slide deck, but rendered on Connect",
+          )
+
